68b1d63f60f292d63e88ad2e3c0ba953d2065178
nicknamemmmm <121141991+nicknamemmmm@users.noreply.github.com>
2023-04-28 22:14:57 +0530

    Add files via upload
    ThirdMavenErrorProject

---
+++ ThirdMavenApplication/src/main/java/com/soft/controller/WelcomeMVN.java
@@ -0,0 +1,21 @@
+package com.soft.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class WelcomeMVN implements Controller
+ {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav =new ModelAndView();
+		mav.setViewName("index");
+		
+		return mav;
+	}
+
+}
