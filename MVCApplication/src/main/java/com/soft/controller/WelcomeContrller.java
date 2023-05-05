f5433933c10d5bc1c1af3d17df2555e0dde07e59
nicknamemmmm <121141991+nicknamemmmm@users.noreply.github.com>
2023-05-05 14:47:40 +0530

    simple mvc
    simpple mvc

---
+++ MVCApplication/src/main/java/com/soft/controller/WelcomeContrller.java
@@ -0,0 +1,20 @@
+package com.soft.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class WelcomeContrller implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("index");
+		
+		return mav;
+	}
+
+}
