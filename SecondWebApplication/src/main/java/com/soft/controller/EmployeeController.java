a7f21a82601fb9b738f881615973ce4b86766325
nicknamemmmm <121141991+nicknamemmmm@users.noreply.github.com>
2023-05-05 14:51:04 +0530

    simple application mvc is hibernate
    simple application mvc is hibernate

---
+++ SecondWebApplication/src/main/java/com/soft/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package com.soft.controller;
+
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class EmployeeController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName("index");
+		
+		return mav;
+	}
+
+	
+
+}
